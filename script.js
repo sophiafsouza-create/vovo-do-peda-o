92f86227326ec96fd1f680e2af09d4b4a3576b14
SOPHIA FANTINI DOS SANTOS SOUZA <55495571851@imaccommand027.local>
2025-12-04 10:53:15 -0300

    menu funcionando

---
+++ script.js
@@ -0,0 +1,42 @@
+let menu = document.getElementById("menu")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
+let sombra = document.getElementById("sombra")
+
+function abreFechaMenu(){
+    //Se o menu está fechado
+    if(window.getComputedStyle(menu).right == "-210px"){
+        //Abrir o menu//
+        menu.style.right = "0"
+
+        //Mostrar icone X "inline faz aparecer"//
+        iconeX.style.display = "inline"
+
+        //Esconde icone barras//
+        iconeBarras.style.display = "none"
+
+        sombra.style.right = "0"
+    }
+    else{
+        //Fechar o menu//
+        menu.style.right = "-210px"
+
+        //Esconder icone X//
+        iconeX.style.display = "none"
+
+        //Mostrar o icone barras//
+        iconeBarras.style.display = "inline"
+
+        sombra.style.right = "-101vw"
+    }   
+}
+
+onresize = () =>{
+    if(window.getComputedStyle(menu).right == "-210px"){
+        //Mostra icone X
+        iconeX.style.display = "none"
+    }else{
+        //Esconde icone barras
+        iconeBarras.style.display = "inline"
+    }
+}
